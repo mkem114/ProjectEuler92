431f603e01da7ce577d5062545af959cd5daa371
mkem114 <20897849+mkem114@users.noreply.github.com>
2023-05-09 00:12:14 +1000

    Minor optimisation

--- Main.kt
+++ Main.kt
@@ -1,40 +1,42 @@
 import java.util.LinkedList
 
 fun main() {
-    val upperLimit = 10_000_000
-    val sets = IntArray(upperLimit + 1)
-    val signifies1Link = 1
-    val signifies89Link = 89
-    sets[1] = signifies1Link
-    sets[89] = signifies89Link
+        val upperLimit = 10_000_000
+        val sets = IntArray(upperLimit + 1)
+        val signifies1Link = 1
+        val signifies89Link = 89
+        var count = 1
+        sets[1] = signifies1Link
+        sets[89] = signifies89Link
 
-    fun updateSets(chain: LinkedList<Int>, set: Int) {
-        chain.forEach { sets[it] = set }
-    }
-
-    (1..upperLimit).forEach { chainStart ->
-        var currentChainLink = chainStart
-        val chain = LinkedList<Int>()
-        while (true) {
-            if (sets[currentChainLink] == signifies1Link) {
-                updateSets(chain, signifies1Link)
-                return@forEach
-            }
-            if (sets[currentChainLink] == signifies89Link) {
-                updateSets(chain, signifies89Link)
-                return@forEach
-            }
-            chain.add(currentChainLink)
-
-            var sumSquare = 0
-            while (currentChainLink > 0) {
-                val remainder = currentChainLink % 10
-                sumSquare += remainder * remainder
-                currentChainLink /= 10
-            }
-            currentChainLink = sumSquare
+        fun updateSets(chain: LinkedList<Int>, set: Int) {
+            chain.forEach { sets[it] = set }
         }
-    }
 
-    println(sets.filter { it == 89 }.size)
+        (1..upperLimit).forEach { chainStart ->
+            var currentChainLink = chainStart
+            val chain = LinkedList<Int>()
+            while (true) {
+                if (sets[currentChainLink] == signifies1Link) {
+                    updateSets(chain, signifies1Link)
+                    return@forEach
+                }
+                if (sets[currentChainLink] == signifies89Link) {
+                    updateSets(chain, signifies89Link)
+                    count += chain.size
+                    return@forEach
+                }
+                chain.add(currentChainLink)
+
+                var sumSquare = 0
+                while (currentChainLink > 0) {
+                    val remainder = currentChainLink % 10
+                    sumSquare += remainder*remainder
+                    currentChainLink /= 10
+                }
+                currentChainLink = sumSquare
+            }
+        }
+
+        println(count)
 }
