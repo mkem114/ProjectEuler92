a10ffd9cd8cce3300382babc9d3ab3c5fda36a24
mkem114 <20897849+mkem114@users.noreply.github.com>
2023-05-09 00:09:39 +1000

    Initial commit

---
+++ Main.kt
@@ -0,0 +1,40 @@
+import java.util.LinkedList
+
+fun main() {
+    val upperLimit = 10_000_000
+    val sets = IntArray(upperLimit + 1)
+    val signifies1Link = 1
+    val signifies89Link = 89
+    sets[1] = signifies1Link
+    sets[89] = signifies89Link
+
+    fun updateSets(chain: LinkedList<Int>, set: Int) {
+        chain.forEach { sets[it] = set }
+    }
+
+    (1..upperLimit).forEach { chainStart ->
+        var currentChainLink = chainStart
+        val chain = LinkedList<Int>()
+        while (true) {
+            if (sets[currentChainLink] == signifies1Link) {
+                updateSets(chain, signifies1Link)
+                return@forEach
+            }
+            if (sets[currentChainLink] == signifies89Link) {
+                updateSets(chain, signifies89Link)
+                return@forEach
+            }
+            chain.add(currentChainLink)
+
+            var sumSquare = 0
+            while (currentChainLink > 0) {
+                val remainder = currentChainLink % 10
+                sumSquare += remainder * remainder
+                currentChainLink /= 10
+            }
+            currentChainLink = sumSquare
+        }
+    }
+
+    println(sets.filter { it == 89 }.size)
+}
